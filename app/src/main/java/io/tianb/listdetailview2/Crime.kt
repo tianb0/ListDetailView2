515c4d48b3abb9050a777c0a8ac2f40f0dd02a47
Tianbo <tianbo@hey.com>
2023-02-04 11:18:59 -0800

    constraint layout

---
+++ app/src/main/java/io/tianb/listdetailview2/Crime.kt
@@ -0,0 +1,12 @@
+package io.tianb.listdetailview2
+
+import java.util.Date
+import java.util.UUID
+
+data class Crime(
+    val id: UUID,
+    val title: String,
+    val date: Date,
+    val isSolved: Boolean
+
+)
---
+++ app/src/main/java/io/tianb/listdetailview2/CrimeListViewModel.kt
@@ -0,0 +1,22 @@
+package io.tianb.listdetailview2
+
+import androidx.lifecycle.ViewModel
+import java.util.*
+
+class CrimeListViewModel : ViewModel() {
+
+    val crimes = mutableListOf<Crime>()
+
+    init {
+        for (i in 0 until 100) {
+            val crime = Crime(
+                id = UUID.randomUUID(),
+                title = "Crime #$i",
+                date = Date(),
+                isSolved = i % 2 == 0
+            )
+
+            crimes += crime
+        }
+    }
+}
